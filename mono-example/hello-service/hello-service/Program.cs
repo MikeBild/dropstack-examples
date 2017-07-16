d6fe4818484f5f8c7d89285b86b61bce5999ed24
Ralf Westphal <info@ralfw.de>
2017-07-16 11:59:11 +0200

    Added functionality with tests
    3 endpoints for hello-greetings, one with a small database

--- mono-example/hello-service/hello-service/Program.cs
+++ mono-example/hello-service/hello-service/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.IO;
+
+using Mono.Unix;
+using Mono.Unix.Native;
 
 using Nancy.Hosting.Self;
 using Nancy;
 
 using NUnit.Framework;
-
+using System.Linq;
 
 namespace helloservice
 {
@@ -17,10 +21,26 @@ namespace helloservice
                 host.Start();
 
                 Console.WriteLine("Running on {0}...", endpointAddress);
-                Console.WriteLine("ENTER to stop service");
-                Console.ReadLine();
+                if (Is_running_on_Mono) {
+                    Console.WriteLine("Ctrl-C to stop service host");
+                    UnixSignal.WaitAny(UnixTerminationSignals);
+                }
+                else {
+                    Console.WriteLine("ENTER to stop service host");
+                    Console.ReadLine();
+                }
             }
         }
+
+
+        private static bool Is_running_on_Mono => Type.GetType("Mono.Runtime") != null;
+
+        private static UnixSignal[] UnixTerminationSignals => new[] {
+            new UnixSignal(Signum.SIGINT),
+            new UnixSignal(Signum.SIGTERM),
+            new UnixSignal(Signum.SIGQUIT),
+            new UnixSignal(Signum.SIGHUP)
+        };
     }
 
 
@@ -28,16 +48,21 @@ namespace helloservice
     {
         public HelloService()
         {
-            Get["/hello"] = _ => {
+            Get["/helloworld"] = _ => {
                 Console.WriteLine("General greeting requested");
                 return "Hello, World!";
             };
 
-            Post["/hello"] = _ =>
-            {
+            Get["/hellome"] = _ => {
                 var name = Request.Query["Name"];
                 Console.WriteLine("Personalized greeting requested for {0}", name);
                 return HelloLogic.Greet_person(name);
+            };
+
+            Post["/hellome"] = _ => {
+			    var name = Request.Query["Name"];
+			    Console.WriteLine("Advanced personalized greeting requested for {0}", name);
+			    return HelloLogic.Greet_visitor(name);
             };
         }
     }
@@ -47,15 +72,69 @@ namespace helloservice
         public static string Greet_person(string name) {
             return $"Hello, {name}!";
         }
+
+        public static string Greet_visitor(string name) {
+            const string VISIT_LOG = "visitors.txt";
+
+            File.AppendAllLines(VISIT_LOG, new[]{name});
+
+            var visits = File.ReadLines(VISIT_LOG);
+            var numberOfVisitsBy = visits.Where(visitor => name == visitor).Count();
+
+            var greeting = "";
+            switch(numberOfVisitsBy) {
+                case 1:
+                    greeting = "Hello, {0}!";
+                    break;
+                case 2:
+                    greeting = "Welcome back, {0}!";
+                    break;
+                default:
+                    greeting = "Hello, my good friend {0}!";
+                    break;
+            }
+
+            return string.Format(greeting, name);
+        }
     }
 
 
     [TestFixture]
     public class test_HelloLogic {
+        [SetUp]
+        public void Setup() {
+            Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
+        }
+
+
         [Test]
         public void Greet_person() {
             var result = HelloLogic.Greet_person("Bruce");
             Assert.AreEqual("Hello, Bruce!", result);
+        }
+
+
+        [Test]
+        public void Greet_visitor() {
+            File.Delete("visitors.txt");
+
+            var result = HelloLogic.Greet_visitor("Peter");
+            Assert.AreEqual("Hello, Peter!", result);
+
+            result = HelloLogic.Greet_visitor("Peter");
+            Assert.AreEqual("Welcome back, Peter!", result);
+
+            result = HelloLogic.Greet_visitor("Peter");
+            Assert.AreEqual("Hello, my good friend Peter!", result);
+
+            result = HelloLogic.Greet_visitor("Mary");
+            Assert.AreEqual("Hello, Mary!", result);
+
+            result = HelloLogic.Greet_visitor("Peter");
+            Assert.AreEqual("Hello, my good friend Peter!", result);
+
+            result = HelloLogic.Greet_visitor("Mary");
+            Assert.AreEqual("Welcome back, Mary!", result);
         }
     }
 }
